49e36142247f730b0b5ea40b2a36febdd104201b
Innnakentiya <innnakentiya@gmail.com>
2024-01-24 19:43:58 +0200

    fix: fix 4-th task by adding more functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,32 +5,59 @@ function copyDir() {
   const newDir = path.join(__dirname, 'files-copy');
   const sourceDir = path.join(__dirname, 'files');
 
-  fs.access(newDir, fs.constants.F_OK, (err) => {
-    if (err) {
-      fs.mkdir(newDir, { recursive: true }, (mkdirErr) => {
-        if (mkdirErr) {
-          console.error(mkdirErr);
-        } else {
-          console.log('Folder has been created');
-          copyFiles(sourceDir, newDir);
-        }
+  fs.access(newDir, fs.constants.F_OK, (newDirErr) => {
+    if (!newDirErr) {
+      deleteFilesInDir(newDir, () => {
+        copyFiles(sourceDir, newDir);
       });
     } else {
-      console.log('Folder already exists');
+      createAndCopy(sourceDir, newDir);
+    }
+  });
+}
+
+function deleteFilesInDir(directory, callback) {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory ${directory}: ${err}`);
+      return;
+    }
+
+    let deletedCount = 0;
+
+    if (files.length === 0) {
+      callback();
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directory, file);
+
+      fs.unlink(filePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error(`Error deleting file ${filePath}: ${unlinkErr}`);
+        } else {
+          console.log(`Deleted file: ${file}`);
+        }
+
+        deletedCount++;
+
+        if (deletedCount === files.length) {
+          callback();
+        }
+      });
+    });
+  });
+}
+
+function createAndCopy(sourceDir, newDir) {
+  fs.mkdir(newDir, { recursive: true }, (mkdirErr) => {
+    if (mkdirErr) {
+      console.error(`Error creating directory ${newDir}: ${mkdirErr}`);
+    } else {
+      console.log('Folder has been created');
       copyFiles(sourceDir, newDir);
     }
   });
-
-  //Check for changes in source directory
-  //   fs.watch(sourceDir, (event, file) => {
-  //     if (event) {
-  //       console.log(`in ${file} has occurred ${event}`);
-  //       // if there are any changes rerun copy function
-  //       copyFiles(sourceDir, newDir);
-  //     } else {
-  //       return;
-  //     }
-  //   });
 }
 
 function copyFiles(sourceDir, newDir) {
@@ -38,23 +65,33 @@ function copyFiles(sourceDir, newDir) {
     if (err) {
       console.error(err);
       return;
-    } else {
-      files.forEach((file) => {
-        const sourceFile = path.join(sourceDir, file);
-        const newFile = path.join(newDir, file);
-
-        fs.copyFile(sourceFile, newFile, (copyErr) => {
-          if (copyErr) {
-            console.error(
-              `Error has occurred while copying ${file} : ${copyErr}`,
-            );
-          } else {
-            // console.log(`${file} has been copied successfully`);
-            return;
-          }
-        });
-      });
     }
+
+    let copiedCount = 0;
+
+    if (files.length === 0) {
+      console.log('No files to copy.');
+    }
+
+    files.forEach((file) => {
+      const sourceFile = path.join(sourceDir, file);
+      const newFile = path.join(newDir, file);
+
+      fs.copyFile(sourceFile, newFile, (copyErr) => {
+        if (copyErr) {
+          console.error(`Error copying file ${file}: ${copyErr}`);
+        } else {
+          console.log(`Copied file: ${file}`);
+        }
+
+        copiedCount++;
+
+        if (copiedCount === files.length) {
+          console.log('All files have been copied.');
+        }
+      });
+    });
   });
 }
+
 copyDir();
